cc191c16dd36355d74400ac3a4191caf6e274d4b
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-01-29 16:56:34 +0900

    Fixing content

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,7 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2ページです</h1>
-      {/* <Link to="/page2/100">URL Parameter</Link> */}
+      <Link to="/page2/100">URL Parameter</Link>
     </div>
   );
 };
